737ea7bb4754e63a77754f42febccaa8d0f1199d
anandchavan24 <anand.chavan@joshsoftware.com>
2024-04-23 15:49:42 +0530

    React | Assignment 8 | Added seperate files for redux

--- src/Components/Feature/Reducers/Pages/TodoItemDetail.tsx
+++ src/Components/Feature/Reducers/Pages/TodoItemDetail.tsx
@@ -1,24 +1,16 @@
 import React, { useEffect, useReducer } from "react";
-import { IToDoRespById } from "../../../Shared/types";
-import useFetchById from "../../Common/useFetchById";
+import { IToDoRespById } from "../../../../Shared/types";
+import useFetchById from "../../../Common/useFetchById";
 import { useParams } from "react-router-dom";
+import { ToDOsItemDetailsReducers } from "../Common/ToDosItemDetailsReducers";
+import { actionTypes } from "../Action/action";
 
 const initialState = { todos: null, loading: true, error: null };
-
-const reducer = (state:any, action:any) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS":
-      return { todos: action.payload, loading: false, error: null };
-    case "FETCH_ERROR":
-      return { todos: null, loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
+;
 
 const TodoItemDetail = () => {
   const { id } = useParams();
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(ToDOsItemDetailsReducers, initialState);
 
   const { data, loading, error }: IToDoRespById = useFetchById(
     "https://jsonplaceholder.typicode.com/todos/" + id
@@ -26,9 +18,9 @@ const TodoItemDetail = () => {
 
   useEffect(() => {
     if (data) {
-      dispatch({ type: "FETCH_SUCCESS", payload: data });
+      dispatch({ type: actionTypes.FETCH_SUCCESS, payload: data });
     } else if (error) {
-      dispatch({ type: "FETCH_ERROR", payload: error });
+      dispatch({ type: actionTypes.FETCH_ERROR, payload: error });
     }
   }, [data, error]);
 
