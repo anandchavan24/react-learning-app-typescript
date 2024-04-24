b8645c97911ee1a874d33809538cf6f74558bda0
anandchavan24 <anand.chavan@joshsoftware.com>
2024-04-24 07:27:13 +0530

    React | Assignment 10 | Add provider in entry point

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import AddToDo from './Components/Feature/AddToDo';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import TodoItemDetail from './Components/Feature/Reducers/Pages/TodoItemDetail';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import TodosListRqComponent from './Components/Feature/ReactQuery/ToDoListRq';
 import TaskForm from './Components/Feature/TaskForm';
 import ToDoList from './Components/Feature/Reducers/Pages/ToDoList';
+import { Provider } from 'react-redux';
+import store from './Components/Feature/Redux/Store';
 
 
 
@@ -14,16 +15,18 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <div className="app">
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/add-todo" Component={AddToDo} />
-            <Route path="/todos/:id" Component={TodoItemDetail} />
-            <Route path="/" Component={ToDoList} />
-            <Route path="/task" Component={TaskForm} />
-          </Routes>
-        </BrowserRouter>      
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/add-todo" Component={AddToDo} />
+              <Route path="/todos/:id" Component={TodoItemDetail} />
+              <Route path="/" Component={ToDoList} />
+              <Route path="/task" Component={TaskForm} />
+            </Routes>
+          </BrowserRouter>      
+        </QueryClientProvider>
+      </Provider>
     </div>
   );
 }
